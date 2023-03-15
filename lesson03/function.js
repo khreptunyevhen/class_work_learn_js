845e31ed89d307702e8b29bbed94b59158924139
Yevhen Khreptun <khreptun.e@gmail.com>
2023-03-15 15:59:25 -0400

    Learned about js functions

---
+++ lesson03/function.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function sum(a, b) {
+    // u = 4;
+    return a + b;
+}
+
+console.log(sum(5, 6));
---
+++ lesson03/strict.js
@@ -0,0 +1,4 @@
+'use strict'
+
+x = 10; // with use strict x is not defined
+console.log(x);
