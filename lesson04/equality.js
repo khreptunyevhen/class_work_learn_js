293272a3c349a87ff47f84d719827eaf3c30a0c2
Yevhen Khreptun <khreptun.e@gmail.com>
2023-03-18 09:27:18 -0400

    learned about increment

---
+++ lesson04/equality.js
@@ -0,0 +1,8 @@
+let x = 5;
+console.log(x !== 5);
+console.log(x !== "5");
+console.log(x !== 8);
+
+// if type not equel OR value not equel
+
+console.log(true !== "1");
---
+++ lesson04/incr_decr.js
@@ -0,0 +1,8 @@
+let a = 1;
+// a++;
+console.log(a++);
+console.log(a);
+let b = 1;
+// ++b;
+console.log(++b);
+console.log(b);
